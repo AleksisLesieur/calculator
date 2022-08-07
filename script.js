3c831114c5fc9f1f934a355577da9279ac0ad000
AleksisLesieur <69734158+AleksisLesieur@users.noreply.github.com>
2022-08-08 02:14:26 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -220,7 +220,70 @@ let equalBtn = document.querySelector("#equal");
 
 let calcTheme = document.querySelector(".calcTheme");
 
-slider.addEventListener("click", function (event) {
+slider.addEventListener("click", clickHandler);
+
+slider.addEventListener("touchend", clickHandler);
+
+// main calculator code
+
+let val;
+
+let calcSymbols = ["+", "-", "/", "*", "."];
+
+for (let item of calcBtns) {
+  item.addEventListener("click", function () {
+    val = item.textContent;
+    if (val !== "RESET" && val !== "DEL" && val !== "=") {
+      input.value += item.textContent;
+    }
+    if (val === "DEL") {
+      input.value = input.value.slice(0, -1);
+    }
+    if (val === "RESET") {
+      input.value = "";
+    }
+    if (val === "=") {
+      input.value = Math.round(eval(input.value) * 100000) / 100000;
+    }
+    if (calcSymbols.includes(val)) {
+      deleteit();
+    }
+    if (input.value.length === 1 && calcSymbols.includes(val)) {
+      input.value = "";
+    }
+    if (input.value === "NaN") {
+      input.value = "";
+    }
+  });
+}
+
+input.addEventListener("keydown", function (event) {
+  if (event.key === "Enter" && input.value.length != 0) {
+    input.value = Math.round(eval(input.value) * 100000) / 100000;
+  }
+});
+
+function deleteit() {
+  let newReg = new RegExp(/([+\-*./]){2,}/gi);
+  inputField.value = inputField.value.replace(newReg, "$1");
+}
+
+function validateMath(s) {
+  let reg = new RegExp(/[^0-9.+\-*/]/gi);
+  return s.replace(reg, "");
+}
+
+input.addEventListener("input", function () {
+  deleteit();
+  let val = input.value;
+  let finalValue = validateMath(val);
+  input.value = finalValue;
+  if (calcSymbols.includes(val)) {
+    input.value = "";
+  }
+});
+
+function clickHandler(event) {
   if (event.target.value == 1) {
     console.log(event.target.value);
 
@@ -228,13 +291,14 @@ slider.addEventListener("click", function (event) {
 
     document.body.style.backgroundColor = bodyBackgroundColor1;
 
-    // injecting into  HTML to change slider, button shadow and hover colors
+    // injecting into HTML to change slider, button shadow and hover colors
 
     styleSlider.innerHTML = `
     .slider::-webkit-slider-thumb { background-color: ${themeSliderColor1}; }
     .button { box-shadow: 0 4px 0 ${buttonItselfShadowColor1};}
     .reset, .del { box-shadow: 0 4px 0 ${delAndResetShadowColor1};}
-    .equal { box-shadow: 0 4px 0 ${equalSignShadowColor1};}`;
+    .equal { box-shadow: 0 4px 0 ${equalSignShadowColor1};}
+    `;
 
     input.style.backgroundColor = inputBackgroundColor1;
 
@@ -247,7 +311,7 @@ slider.addEventListener("click", function (event) {
     for (let i = 0; i < calcBtns.length; i++) {
       calcBtns[i].style.color = mainButtonColor1;
       calcBtns[i].style.backgroundColor = buttonIstelfBackgroundColor1;
-      calcBtns[i].style.boxShadow = buttonItselfShadowColor1;
+      // calcBtns[i].style.boxShadow = buttonItselfShadowColor1;
       calcBtns[i].addEventListener("mouseenter", function () {
         calcBtns[i].style.backgroundColor = "white";
       });
@@ -271,11 +335,6 @@ slider.addEventListener("click", function (event) {
     delBtn.style.color = "white";
     equalBtn.style.color = "white";
 
-    // changing shadow colors
-
-    // resetBtn.style.boxShadow = delAndResetShadowColor1;
-    // delBtn.style.boxShadow = delAndResetShadowColor1;
-    // equalBtn.style.boxShadow = equalSignShadowColor1;
     resetBtn.addEventListener("mouseenter", function () {
       resetBtn.style.backgroundColor = delAndResetButtonHover1;
     });
@@ -428,283 +487,4 @@ slider.addEventListener("click", function (event) {
       equalBtn.style.backgroundColor = themeSliderColor3;
     });
   }
-});
-
-slider.addEventListener("touchend", function (event) {
-  if (event.target.value == 1) {
-    console.log(event.target.value);
-
-    // changing background colors
-
-    document.body.style.backgroundColor = bodyBackgroundColor1;
-
-    // injecting into  HTML to change slider, button shadow and hover colors
-
-    styleSlider.innerHTML = `
-    .slider::-webkit-slider-thumb { background-color: ${themeSliderColor1}; }
-    .button { box-shadow: 0 4px 0 ${buttonItselfShadowColor1};}
-    .reset, .del { box-shadow: 0 4px 0 ${delAndResetShadowColor1};}
-    .equal { box-shadow: 0 4px 0 ${equalSignShadowColor1};}`;
-
-    input.style.backgroundColor = inputBackgroundColor1;
-
-    calcBtnBackground.style.backgroundColor = aroundButtonBackgroundColor1;
-
-    slider.style.backgroundColor = aroundButtonBackgroundColor1;
-
-    // changing button colors
-
-    for (let i = 0; i < calcBtns.length; i++) {
-      calcBtns[i].style.color = mainButtonColor1;
-      calcBtns[i].style.backgroundColor = buttonIstelfBackgroundColor1;
-      calcBtns[i].style.boxShadow = buttonItselfShadowColor1;
-      calcBtns[i].addEventListener("mouseenter", function () {
-        calcBtns[i].style.backgroundColor = "white";
-      });
-      calcBtns[i].addEventListener("mouseleave", function () {
-        calcBtns[i].style.backgroundColor = buttonIstelfBackgroundColor1;
-      });
-    }
-
-    resetBtn.style.backgroundColor = delAndResetBackgroundColor1;
-    delBtn.style.backgroundColor = delAndResetBackgroundColor1;
-    equalBtn.style.backgroundColor = themeSliderColor1;
-
-    for (let i = 0; i < buttonNumber.length; i++) {
-      buttonNumber[i].style.color = "white";
-    }
-
-    calcText.style.color = "white";
-    theme.style.color = "white";
-    input.style.color = "white";
-    resetBtn.style.color = "white";
-    delBtn.style.color = "white";
-    equalBtn.style.color = "white";
-
-    // changing shadow colors
-
-    // resetBtn.style.boxShadow = delAndResetShadowColor1;
-    // delBtn.style.boxShadow = delAndResetShadowColor1;
-    // equalBtn.style.boxShadow = equalSignShadowColor1;
-    resetBtn.addEventListener("mouseenter", function () {
-      resetBtn.style.backgroundColor = delAndResetButtonHover1;
-    });
-    resetBtn.addEventListener("mouseleave", function () {
-      resetBtn.style.backgroundColor = delAndResetBackgroundColor1;
-    });
-    delBtn.addEventListener("mouseenter", function () {
-      delBtn.style.backgroundColor = delAndResetButtonHover1;
-    });
-    delBtn.addEventListener("mouseleave", function () {
-      delBtn.style.backgroundColor = delAndResetBackgroundColor1;
-    });
-    equalBtn.addEventListener("mouseenter", function () {
-      equalBtn.style.backgroundColor = equalButtonHover1;
-    });
-    equalBtn.addEventListener("mouseleave", function () {
-      equalBtn.style.backgroundColor = equalButtonHover1;
-    });
-  }
-  if (event.target.value == 2) {
-    console.log(event.target.value);
-    // changing background colors
-    document.body.style.backgroundColor = bodyBackgroundColor2;
-
-    styleSlider.innerHTML = `
-    .slider::-webkit-slider-thumb { background-color: ${themeSliderColor2}; }
-    .button { box-shadow: 0 4px 0 ${buttonItselfShadowColor2};}
-    .reset, .del { box-shadow: 0 4px 0 ${delAndResetShadowColor2};}
-    .equal { box-shadow: 0 4px 0 ${equalSignShadowColor2};}`;
-
-    input.style.backgroundColor = inputBackgroundColor2;
-
-    calcBtnBackground.style.backgroundColor = aroundButtonBackgroundColor2;
-
-    slider.style.backgroundColor = aroundButtonBackgroundColor2;
-
-    // changing button colors
-
-    for (let i = 0; i < calcBtns.length; i++) {
-      calcBtns[i].style.color = mainButtonColor2;
-      calcBtns[i].style.backgroundColor = buttonIstelfBackgroundColor2;
-      calcBtns[i].style.boxShadow = buttonItselfShadowColor2;
-      calcBtns[i].addEventListener("mouseenter", function () {
-        calcBtns[i].style.backgroundColor = "white";
-      });
-      calcBtns[i].addEventListener("mouseleave", function () {
-        calcBtns[i].style.backgroundColor = buttonIstelfBackgroundColor2;
-      });
-    }
-
-    for (let i = 0; i < buttonNumber.length; i++) {
-      buttonNumber[i].style.color = mainButtonColor2;
-    }
-
-    resetBtn.style.backgroundColor = delAndResetBackgroundColor2;
-    delBtn.style.backgroundColor = delAndResetBackgroundColor2;
-    equalBtn.style.backgroundColor = themeSliderColor2;
-
-    calcText.style.color = mainButtonColor2;
-
-    theme.style.color = mainButtonColor2;
-    input.style.color = mainButtonColor2;
-    resetBtn.style.color = "white";
-    delBtn.style.color = "white";
-    equalBtn.style.color = "white";
-
-    resetBtn.addEventListener("mouseenter", function () {
-      resetBtn.style.backgroundColor = delAndResetButtonHover2;
-    });
-    resetBtn.addEventListener("mouseleave", function () {
-      resetBtn.style.backgroundColor = delAndResetBackgroundColor2;
-    });
-    delBtn.addEventListener("mouseenter", function () {
-      delBtn.style.backgroundColor = delAndResetButtonHover2;
-    });
-    delBtn.addEventListener("mouseleave", function () {
-      delBtn.style.backgroundColor = delAndResetBackgroundColor2;
-    });
-    equalBtn.addEventListener("mouseenter", function () {
-      equalBtn.style.backgroundColor = equalButtonHover2;
-    });
-    equalBtn.addEventListener("mouseleave", function () {
-      equalBtn.style.backgroundColor = equalButtonHover2;
-    });
-  }
-
-  if (event.target.value == 3) {
-    console.log(event.target.value);
-    document.body.style.backgroundColor = bodyBackgroundColor3;
-
-    slider.style.backgroundColor = inputBackgroundColor3;
-
-    styleSlider.innerHTML = `
-    .slider::-webkit-slider-thumb { background-color: ${themeSliderColor3}; }
-    .button { box-shadow: 0 4px 0 ${buttonItselfShadowColor3};}
-    .reset, .del { box-shadow: 0 4px 0 ${delAndResetShadowColor3};}
-    .equal { box-shadow: 0 4px 0 ${equalSignShadowColor3};}`;
-
-    input.style.backgroundColor = inputBackgroundColor3;
-
-    calcBtnBackground.style.backgroundColor = inputBackgroundColor3;
-
-    slider.style.backgroundColor = inputBackgroundColor3;
-
-    // changing button colors
-
-    for (let i = 0; i < calcBtns.length; i++) {
-      calcBtns[i].style.color = mainButtonColor3;
-      calcBtns[i].style.backgroundColor = buttonIstelfBackgroundColor3;
-      calcBtns[i].style.boxShadow = buttonItselfShadowColor3;
-      calcBtns[i].addEventListener("mouseenter", function () {
-        calcBtns[i].style.backgroundColor = mainButtonsHover3;
-      });
-      calcBtns[i].addEventListener("mouseleave", function () {
-        calcBtns[i].style.backgroundColor = buttonIstelfBackgroundColor3;
-      });
-    }
-
-    for (let i = 0; i < buttonNumber.length; i++) {
-      buttonNumber[i].style.color = mainButtonColor3;
-    }
-
-    calcText.style.color = mainButtonColor3;
-    theme.style.color = mainButtonColor3;
-    input.style.color = mainButtonColor3;
-    resetBtn.style.color = "white";
-    delBtn.style.color = "white";
-    equalBtn.style.color = equalSignColor3;
-
-    resetBtn.style.backgroundColor = delAndResetBackgroundColor3;
-    delBtn.style.backgroundColor = delAndResetBackgroundColor3;
-    equalBtn.style.backgroundColor = themeSliderColor3;
-
-    resetBtn.addEventListener("mouseenter", function () {
-      resetBtn.style.backgroundColor = delAndResetButtonHover3;
-    });
-    resetBtn.addEventListener("mouseleave", function () {
-      resetBtn.style.backgroundColor = delAndResetBackgroundColor3;
-    });
-    delBtn.addEventListener("mouseenter", function () {
-      delBtn.style.backgroundColor = delAndResetButtonHover3;
-    });
-    delBtn.addEventListener("mouseleave", function () {
-      delBtn.style.backgroundColor = delAndResetBackgroundColor3;
-    });
-    equalBtn.addEventListener("mouseenter", function () {
-      equalBtn.style.backgroundColor = equalButtonHover3;
-    });
-    equalBtn.addEventListener("mouseleave", function () {
-      equalBtn.style.backgroundColor = themeSliderColor3;
-    });
-  }
-});
-
-// main calculator code
-
-let val;
-
-for (let item of calcBtns) {
-  item.addEventListener("click", function () {
-    val = item.textContent;
-    if (val !== "RESET" && val !== "DEL" && val !== "=") {
-      input.value += item.textContent;
-    }
-    if (val === "DEL" || val === "RESET") {
-      input.value = "";
-    }
-    if (val === "=") {
-      input.value = Math.round(eval(input.value) * 100000) / 100000;
-    }
-    if (
-      val === "+" ||
-      val === "-" ||
-      val === "/" ||
-      val === "*" ||
-      val === "."
-    ) {
-      deleteit();
-    }
-    if (
-      input.value.length === 1 &&
-      (val === "+" || val === "-" || val === "/" || val === "*" || val === ".")
-    ) {
-      input.value = "";
-    }
-    if (input.value === "NaN") {
-      input.value = "";
-    }
-  });
 }
-
-input.addEventListener("keydown", function (event) {
-  if (event.key === "Enter" && input.value.length != 0) {
-    input.value = Math.round(eval(input.value) * 100000) / 100000;
-  }
-});
-
-function deleteit() {
-  let newReg = new RegExp(/([+\-*./]){2,}/gi);
-  inputField.value = inputField.value.replace(newReg, "$1");
-}
-
-function validateMath(s) {
-  let reg = new RegExp(/[^0-9.+\-*/]/gi);
-  return s.replace(reg, "");
-}
-
-input.addEventListener("input", function () {
-  deleteit();
-  let val = input.value;
-  let finalValue = validateMath(val);
-  input.value = finalValue;
-  if (
-    input.value === "+" ||
-    input.value === "-" ||
-    input.value === "/" ||
-    input.value === "*" ||
-    input.value === "."
-  ) {
-    input.value = "";
-  }
-});
